3cf6fe1db4fb0ef983a7ef6ff55f58cf7fbe47fb
hedzofficial <npkrdy@gmail.com>
2025-09-06 05:30:06 +0530

    Auto-commit: update trilogy site

--- update_git.bat
+++ update_git.bat
@@ -1,16 +1,19 @@
 @echo off
 cd /d "%~dp0"
 
+REM --- Rename files if they have unwanted .txt extension ---
+for %%f in (*.html.txt) do ren "%%f" "%%~nf.html"
+for %%f in (*.css.txt) do ren "%%f" "%%~nf.css"
+for %%f in (*.js.txt) do ren "%%f" "%%~nf.js"
+
 REM Stage all changes
 git add .
 
 REM Commit with default message
-git commit -m "Auto-commit: update hedz book"
+git commit -m "Auto-commit: update trilogy site"
 
-REM Check if commit succeeded
 IF %ERRORLEVEL% NEQ 0 (
     echo Nothing to commit or commit failed.
-    REM continue anyway
 )
 
 REM Push to GitHub
