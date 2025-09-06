5ba0b35b00b2de1ba863664f19863a100fd4372b
hedzofficial <npkrdy@gmail.com>
2025-09-06 06:08:11 +0530

    Auto-commit: update trilogy site

--- update_git.bat
+++ update_git.bat
@@ -1,19 +1,16 @@
 @echo off
 cd /d "%~dp0"
 
-REM Rename HTML, CSS, JS files recursively in subfolders
-for /r %%f in (*.html.txt) do ren "%%f" "%%~nf.html"
-for /r %%f in (*.css.txt) do ren "%%f" "%%~nf.css"
-for /r %%f in (*.js.txt) do ren "%%f" "%%~nf.js"
-
 REM Stage all changes
 git add .
 
 REM Commit with default message
 git commit -m "Auto-commit: update trilogy site"
 
+REM Check if commit succeeded
 IF %ERRORLEVEL% NEQ 0 (
     echo Nothing to commit or commit failed.
+    REM continue anyway
 )
 
 REM Push to GitHub
